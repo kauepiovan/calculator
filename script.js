29a10837391f31390f6787c23aea8b056335154a
kaue piovan <kaupiovan@gmail.com>
2024-02-11 02:24:42 -0300

    formated

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 // show the digiteds characters
 let number1, number;
-let operation
+let operation;
 
 function show(character) {
-  document.getElementById('output').value += character;
+  document.getElementById("output").value += character;
 }
-
 
 // set the operations
 function setOperation(input) {
-  if (document.getElementById('output').value.length > 0) {
+  if (document.getElementById("output").value.length > 0) {
     number1 = setNumberOne();
     clearOutput();
   }
 
   switch (input) {
-    case 'sum':
-      operation = 'sum'
+    case "sum":
+      operation = "sum";
       break;
 
-    case 'subtraction':
-      operation = 'subtraction'
+    case "subtraction":
+      operation = "subtraction";
       break;
 
-    case 'multiplication':
-      operation = 'multiplication'
+    case "multiplication":
+      operation = "multiplication";
       break;
 
-    case 'division':
-      operation = 'division'
+    case "division":
+      operation = "division";
       break;
 
     default:
@@ -40,76 +39,74 @@ function setOperation(input) {
 function equals() {
   let result;
 
-  if (document.getElementById('output').value.length > 0) {
+  if (document.getElementById("output").value.length > 0) {
     number2 = setNumberOne();
-  }
-  else{
+  } else {
     number2 = 0;
   }
 
-  if(number1 == undefined || number == ''){
+  if (number1 == undefined || number == "") {
     number1 = 0;
   }
 
   switch (operation) {
-    case 'sum':
+    case "sum":
       result = parseInt(number1) + parseInt(number2);
       break;
 
-    case 'subtraction':
+    case "subtraction":
       result = parseInt(number1) - parseInt(number2);
       break;
 
-    case 'multiplication':
+    case "multiplication":
       result = parseInt(number1) * parseInt(number2);
       break;
 
-    case 'division':
+    case "division":
       result = parseInt(number1) / parseInt(number2);
       break;
 
     case undefined:
-    case '':
+    case "":
       result = 0;
       break;
 
-      default:
-        document.getElementById('output').value = 'error'
+    default:
+      document.getElementById("output").value = "error";
       break;
   }
 
-  document.getElementById('output').value = result;
+  document.getElementById("output").value = result;
 }
 
 // show the results
 
 // clear output
 function deleteAllOutput() {
-  document.getElementById('output').value = '';
-  number1 = '';
-  number2 = '';
-  operation = '';
+  document.getElementById("output").value = "";
+  number1 = "";
+  number2 = "";
+  operation = "";
 }
 
 function clearOutput() {
-  document.getElementById('output').value = '';
+  document.getElementById("output").value = "";
 }
 
 // get the number
 function setNumberOne() {
   let number;
 
-  if (document.getElementById('output').value.length > 0) {
+  if (document.getElementById("output").value.length > 0) {
     number = getOutput();
   }
-  console.log(number)
+  console.log(number);
   return number;
 }
 
-
 function getOutput() {
-  let number = '';
-  let output = document.getElementById('output').value;
+  let number = "";
+  let output = document.getElementById("output").value;
 
   for (character of output) {
     if (!isNaN(character)) {
